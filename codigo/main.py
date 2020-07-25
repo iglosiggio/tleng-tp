efbb043f815e76c3859cef642bbfad6a40225932
Ignacio Losiggio <iglosiggio@gmail.com>
2020-07-25 16:28:44 -0300

    codigo: Initial complete grammar

--- codigo/main.py
+++ codigo/main.py
@@ -12,45 +12,54 @@ tokens = (
     'GAME_RESULT',
     'WORD',
 )
-t_ignore = ' \t\r\n'
+
+t_ignore = ' \t\r'
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
 
 # Poner todos los tokens en funciones fuerza la prioridad
 # para desambiguar entre ellos.
 
 def t_BEGIN_DESCRIPTOR(t):
-    '\['
+    r'\['
     return t
 
 def t_DESCRIPTOR_VALUE(t):
-    '"[^"]*"'
+    r'"[^"]*"'
     return t
 
 def t_END_DESCRIPTOR(t):
-    ']'
+    r']'
     return t
 
 def t_BEGIN_COMMENT(t):
-    '{'
+    r'{'
     return t
 
 def t_END_COMMENT(t):
-    '}'
+    r'}'
     return t
 
 def t_MOVE_NUMBER(t):
-    '[1-9][0-9]*\.(\.\.)?'
+    r'[1-9][0-9]*\.(\.\.)?'
     return t
 
 def t_MOVE(t):
-    '([PNBRQK]?[a-h]?[0-9]?x?[a-h][1-8]|O-O|O-O-O)[+#]?'
+    r'([PNBRQK]?[a-h]?[0-9]?x?[a-h][1-8]|O-O-O|O-O)[+#]?'
     return t
 
 def t_GAME_RESULT(t):
-    '1-0|0-1|1/2-1/2'
+    r'1-0|0-1|1/2-1/2'
     return t
 
 def t_WORD(t):
-    '[^ \t\r\n}]+'
+    r'[^ \t\r\n}]+'
     return t
 
 lexer = lex.lex()
@@ -58,10 +67,8 @@ lexer = lex.lex()
 import ply.yacc as yacc
 
 def p_pgn_file(p):
-    'file : descriptor_list'
-    p[0] = { 'descriptor': p[1] }
-    #'file : descriptor_list game'
-    #p[0] = { 'descriptor': p[1], 'game': p[2] }
+    'file : descriptor_list game'
+    p[0] = { 'descriptor': p[1], 'game': p[2] }
 
 def p_descriptor_list(p):
     '''descriptor_list : descriptor descriptor_list
@@ -105,13 +112,46 @@ def p_move(p):
 
 def p_move_content(p):
     '''move_content : MOVE
-                    | MOVE move_content
-                    | comment'''
+                    | comment
+                    | MOVE move_content'''
+    if len(p) == 3:
+        p[0] = [p[1]] + p[2]
+    else:
+        p[0] = [p[1]]
 
 def p_comment(p):
-    'comment : BEGIN_COMMENT END_COMMENT'
-    p[0] = ''
+    'comment : BEGIN_COMMENT comment_words_list END_COMMENT'
+    p[0] = p[2]
 
+def p_comment_words_list(p):
+    '''comment_words_list : comment_word comment_words_list
+                          | comment_word'''
+    if len(p) == 3:
+        p[0] = [p[1]] + p[2]
+    else:
+        p[0] = [p[1]]
+
+def p_comment_word(p):
+    '''comment_word : comment
+                    | any_comment_token'''
+    p[0] = p[1]
+
+
+def p_any_comment_token(p):
+    '''any_comment_token : BEGIN_DESCRIPTOR
+                         | DESCRIPTOR_VALUE
+                         | END_DESCRIPTOR
+                         | MOVE_NUMBER
+                         | MOVE
+                         | GAME_RESULT
+                         | WORD'''
+    p[0] = p[1]
+
+def p_error(p):
+    if p:
+        print("yacc: Syntax error at line %s, token=%s (%s)" % (p.lineno, p.type, p.value))
+    else:
+        print("Syntax error at EOF")
 
 parser = yacc.yacc()
 
