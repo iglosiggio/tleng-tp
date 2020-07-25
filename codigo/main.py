cb4d25ad7d06b63f8061294c2f1a7597894fcb79
Ignacio Losiggio <iglosiggio@gmail.com>
2020-07-25 15:59:04 -0300

    codigo: Grammar for descriptors

--- codigo/main.py
+++ codigo/main.py
@@ -3,7 +3,6 @@ import ply.lex as lex
 # Lista de terminales
 tokens = (
     'BEGIN_DESCRIPTOR',
-    'DESCRIPTOR_NAME',
     'DESCRIPTOR_VALUE',
     'END_DESCRIPTOR',
     'BEGIN_COMMENT',
@@ -56,5 +55,68 @@ def t_WORD(t):
 
 lexer = lex.lex()
 
+import ply.yacc as yacc
+
+def p_pgn_file(p):
+    'file : descriptor_list'
+    p[0] = { 'descriptor': p[1] }
+    #'file : descriptor_list game'
+    #p[0] = { 'descriptor': p[1], 'game': p[2] }
+
+def p_descriptor_list(p):
+    '''descriptor_list : descriptor descriptor_list
+                       | descriptor'''
+    if len(p) == 3:
+        p[0] = [p[1]] + p[2]
+    else:
+        p[0] = [p[1]]
+
+def p_descriptor(p):
+    'descriptor : BEGIN_DESCRIPTOR any_token DESCRIPTOR_VALUE END_DESCRIPTOR'
+    p[0] = { 'name': p[2], 'value': p[3] }
+
+def p_any_token(p):
+    '''any_token : BEGIN_DESCRIPTOR
+                 | DESCRIPTOR_VALUE
+                 | END_DESCRIPTOR
+                 | BEGIN_COMMENT
+                 | END_COMMENT
+                 | MOVE_NUMBER
+                 | MOVE
+                 | GAME_RESULT
+                 | WORD'''
+    p[0] = p[1]
+
+def p_game(p):
+    'game : move_list GAME_RESULT'
+    p[0] = { 'moves': p[1], 'result': p[2] }
+
+def p_move_list(p):
+    '''move_list : move move_list
+                 | move'''
+    if len(p) == 3:
+        p[0] = [p[1]] + p[2]
+    else:
+        p[0] = [p[1]]
+
+def p_move(p):
+    'move : MOVE_NUMBER move_content'
+    p[0] = (p[1], p[2])
+
+def p_move_content(p):
+    '''move_content : MOVE
+                    | MOVE move_content
+                    | comment'''
+
+def p_comment(p):
+    'comment : BEGIN_COMMENT END_COMMENT'
+    p[0] = ''
+
+
+parser = yacc.yacc()
+
 if __name__ == '__main__':
-    lex.runmain()
+    import sys
+    s = sys.stdin.read()
+    result = parser.parse(s)
+    print(result)
